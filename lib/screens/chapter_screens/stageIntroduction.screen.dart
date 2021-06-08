9c3cf2845883c4111d3b4141c41cff13aecb7e07
Danthology96 <daniel.andres.novillo@gmail.com>
2021-06-08 17:12:38 -0500

    fix(StageIntroductionScreen): Making the content scroll-able

--- lib/screens/chapter_screens/stageIntroduction.screen.dart
+++ lib/screens/chapter_screens/stageIntroduction.screen.dart
@@ -25,9 +25,9 @@ class StageIntroductionScreen extends StatelessWidget {
         child: Stack(
           children: [
             ChapterBackgroundWidget(
-                backgroundColor: ColorsApp.backgroundOrange,
-                relieve: _backgroundDecoration(),
-                hasBanner: true,
+              backgroundColor: ColorsApp.backgroundOrange,
+              relieve: _backgroundDecoration(),
+              hasBanner: true,
             ),
             _routeCurve(),
             _stageBody(size),
@@ -37,6 +37,7 @@ class StageIntroductionScreen extends StatelessWidget {
       bottomNavigationBar: CustomNavigationBar(),
     );
   }
+
   _backgroundDecoration() {
     return Container(
       alignment: Alignment.bottomCenter,
@@ -49,7 +50,8 @@ class StageIntroductionScreen extends StatelessWidget {
       ),
     );
   }
-   _routeCurve() {
+
+  _routeCurve() {
     return Container(
       alignment: Alignment.bottomCenter,
       width: double.infinity,
@@ -62,34 +64,35 @@ class StageIntroductionScreen extends StatelessWidget {
       ),
     );
   }
-    
 
+  ///scroll-able content
   _stageBody(Size size) {
-    double bodyContainerWidth = size.height*0.38;
-    double bodyContainerHeight = size.height*0.75;
-    double bodyMarginLeft= size.width*0.10;
-    double bodyMarginTop= size.width*0.30;
-    double firstImageContainerWidth = bodyContainerWidth*0.3;
-    double firstImageContainerHeight = bodyContainerHeight*0.12;
-    double firstImageMarginRight =bodyContainerWidth*0.55;
+    double bodyContainerWidth = size.height * 0.48;
+    double bodyContainerHeight = size.height * 0.75;
+    double bodyMarginLeft = size.width * 0.10;
+    double bodyMarginTop = size.width * 0.30;
+    double firstImageContainerWidth = bodyContainerWidth * 0.3;
+    double firstImageContainerHeight = bodyContainerHeight * 0.12;
+    double firstImageMarginRight = bodyContainerWidth * 0.55;
     double spacedBodyContainers = bodyContainerHeight * 0.015;
-    double secondImageContainerWidth = bodyContainerWidth*0.55;
-    double secondImageContainerHeight = bodyContainerHeight*0.20;
-    double secondImageMarginRight =bodyContainerWidth*0.35;
+    double secondImageContainerWidth = bodyContainerWidth * 0.55;
+    double secondImageContainerHeight = bodyContainerHeight * 0.20;
+    double secondImageMarginRight = bodyContainerWidth * 0.35;
     return Container(
       alignment: Alignment.topLeft,
       width: bodyContainerWidth,
       height: bodyContainerHeight,
-      margin: EdgeInsets.only(left: bodyMarginLeft,top: bodyMarginTop),
+      margin: EdgeInsets.only(
+          left: bodyMarginLeft, right: bodyMarginLeft, top: bodyMarginTop),
       // decoration: BoxDecoration(
       //   border: Border.all(color: Colors.blueAccent)
       // ),
-      child: Column(
+      child: ListView(
         children: <Widget>[
           Container(
             width: firstImageContainerWidth,
             height: firstImageContainerHeight,
-            margin: EdgeInsets.only(right: firstImageMarginRight),            
+            margin: EdgeInsets.only(right: firstImageMarginRight),
             // decoration: BoxDecoration(
             //   border: Border.all(color: Colors.white)
             // ),
@@ -98,32 +101,33 @@ class StageIntroductionScreen extends StatelessWidget {
                 'assets/backgrounds/decorations/logo_stage_prin.png',
               ),
             ),
-
           ),
           SizedBox(height: spacedBodyContainers),
           Container(
-            width: bodyContainerWidth*0.9,
-            height: firstImageContainerHeight,
-            // decoration: BoxDecoration(
-            //   border: Border.all(color: Colors.white)
-            // ),
-            child: Text('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolo remque laudantium, totam rem aperiam',
-            style: korolevFont.headline4?.apply(
-            fontSizeFactor: size.height * 0.00068, fontWeightDelta: 2),textAlign: TextAlign.left,)
-
-          ),
-          
+              width: bodyContainerWidth * 0.9,
+              height: firstImageContainerHeight,
+              // decoration: BoxDecoration(
+              //   border: Border.all(color: Colors.white)
+              // ),
+              child: Text(
+                'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolo remque laudantium, totam rem aperiam',
+                style: korolevFont.headline4?.apply(
+                    fontSizeFactor: size.height * 0.00068, fontWeightDelta: 2),
+                textAlign: TextAlign.left,
+              )),
           SizedBox(height: spacedBodyContainers),
           Container(
-            width: bodyContainerWidth*0.9,
-            height: bodyContainerHeight*0.3,
-            // decoration: BoxDecoration(
-            //   border: Border.all(color: Colors.white)
-            // ),
-            child: Text('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione',
-            style: korolevFont.bodyText1?.apply(
-            fontSizeFactor: size.height * 0.0012, fontWeightDelta: 0),textAlign: TextAlign.left,)
-          ),
+              width: bodyContainerWidth * 0.9,
+              height: bodyContainerHeight * 0.3,
+              // decoration: BoxDecoration(
+              //   border: Border.all(color: Colors.white)
+              // ),
+              child: Text(
+                'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione',
+                style: korolevFont.bodyText1?.apply(
+                    fontSizeFactor: size.height * 0.0012, fontWeightDelta: 0),
+                textAlign: TextAlign.left,
+              )),
           SizedBox(height: spacedBodyContainers),
           Container(
             width: secondImageContainerWidth,
@@ -131,7 +135,7 @@ class StageIntroductionScreen extends StatelessWidget {
             // decoration: BoxDecoration(
             //   border: Border.all(color: Colors.white)
             // ),
-            margin: EdgeInsets.only(right: secondImageMarginRight),  
+            margin: EdgeInsets.only(right: secondImageMarginRight),
             child: Image(
               image: AssetImage(
                 'assets/backgrounds/decorations/logo_stage_secun.png',
@@ -140,15 +144,17 @@ class StageIntroductionScreen extends StatelessWidget {
           ),
           SizedBox(height: spacedBodyContainers),
           Container(
-            width: bodyContainerWidth*0.9,
-            height: bodyContainerHeight*0.18,
-            // decoration: BoxDecoration(
-            //   border: Border.all(color: Colors.white)
-            // ),
-            child: Text('Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora',
-            style: korolevFont.bodyText2?.apply(
-            fontSizeFactor: size.height * 0.0012, fontWeightDelta: 0),textAlign: TextAlign.left,)
-          )
+              width: bodyContainerWidth * 0.9,
+              height: bodyContainerHeight * 0.18,
+              // decoration: BoxDecoration(
+              //   border: Border.all(color: Colors.white)
+              // ),
+              child: Text(
+                'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora',
+                style: korolevFont.bodyText2?.apply(
+                    fontSizeFactor: size.height * 0.0012, fontWeightDelta: 0),
+                textAlign: TextAlign.left,
+              ))
         ],
       ),
     );
