01f52b49d633f83b12e914331921cd71ccf4bf6a
Danthology96 <daniel.andres.novillo@gmail.com>
2021-06-09 19:07:54 -0500

    add: Adding 4 icons and an asset image when no image is provided. feat (lectures-list-item_widget): Creating a custom widget that returns  a book resource container. feat (online-resources-grid-item_widget): Creating a custom widget that returns an online resource container. fix(resources.screen.dart): Adding, online resources section

--- lib/screens/chapter_screens/resources.screen.dart
+++ lib/screens/chapter_screens/resources.screen.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:lab_movil_2222/shared/widgets/chapter-title-section.dart';
 import 'package:lab_movil_2222/shared/widgets/chapter_background_widget.dart';
 import 'package:lab_movil_2222/shared/widgets/custom_navigation_bar.dart';
+import 'package:lab_movil_2222/shared/widgets/lectures-list-item_widget.dart';
+import 'package:lab_movil_2222/shared/widgets/online-resources-grid-item_widget.dart';
 import 'package:lab_movil_2222/themes/colors.dart';
-import 'package:lab_movil_2222/themes/textTheme.dart';
 
 class ResourcesScreen extends StatelessWidget {
   @override
@@ -45,79 +46,106 @@ class ResourcesScreen extends StatelessWidget {
     );
   }
 
+  ///body of the screen
   _resourcesContent(Size size) {
     double bodyMarginWidth = size.width * 0.30;
     double bodyMarginHeight = size.height * 0.03;
+
+    ///sizing the container to the mobile
     return Container(
       margin: EdgeInsets.only(
         top: bodyMarginWidth,
         right: bodyMarginHeight,
       ),
-      // decoration: BoxDecoration(border: Border.all(color: Colors.white)),
+
+      ///Listview of the whole screen
       child: ListView(
         children: [
+          ///chapter title screen widgets
           ChapterTitleSection(
             title: 'LECTURAS',
           ),
           SizedBox(
-            height: 10,
+            height: 20,
           ),
-          _booksBody([3, 2, 5, 6]),
+
+          ///list of the bodys (json expected)
+          _booksBody([3, 2]),
           ChapterTitleSection(
             title: 'RECURSOS ONLINE',
           ),
           SizedBox(
-            height: 10,
+            height: 20,
           ),
-          ChapterTitleSection(
-            title: 'Objetivo'.toUpperCase(),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          ChapterTitleSection(
-            title: 'Contenidos'.toUpperCase(),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          ChapterTitleSection(
-            title: 'Competencias'.toUpperCase(),
-          ),
+
+          ///calling the body of online resources, expected a json
+          _onlineResourcesBody([
+            2,
+            3,
+            2,
+            1,
+            2,
+          ]),
         ],
       ),
     );
   }
 
+  ///books body method
   _booksBody(List list) {
+    ///main container
     return Container(
+      ///general left padding 25
       padding: EdgeInsets.only(left: 25),
-      height: list.length * 90,
+
+      ///To resize the parent container of the list of books
+      height: (list.length) * 150,
       child: ListView.builder(
           physics: NeverScrollableScrollPhysics(),
           itemCount: list.length,
           itemBuilder: (context, index) {
-            return ListTile(
-              leading: FadeInImage(
-                placeholder: AssetImage('assets/gifs/giphy.gif'),
-                image: NetworkImage(
-                    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/The_Portrait_of_a_lady_cover.jpg/220px-The_Portrait_of_a_lady_cover.jpg'),
-                fit: BoxFit.cover,
-              ),
-              title: Text(
-                'TÍTULO LIBRO',
-                style: korolevFont.headline6,
-              ),
-              subtitle: Text(
-                'Autor | Editorial | Año',
-                style: korolevFont.bodyText2,
-              ),
-              isThreeLine: true,
-              onTap: () {
-                print('presionó');
-              },
+            ///bringing a book resource per item in the list
+            return LecturesListItem(
+              imageURL:
+                  'https://www.pngarts.com/files/8/Blank-Book-Cover-PNG-Picture.png',
+              title: 'El Principito valiente',
+              author: 'Daniel Novillo',
+              year: '1979',
+              editorial: 'Editorial San José Ignacio de Barravanes',
+              review:
+                  'Tenim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia conseunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
             );
           }),
+    );
+  }
+
+  ///Method of the online resources
+  _onlineResourcesBody(List list) {
+    ///main container
+    return Container(
+      padding: EdgeInsets.only(left: 25),
+
+      ///To resize the parent container of the online resources grid
+      height: (list.length) * 110,
+
+      ///Creates a grid with the necesary online resources
+      child: GridView.builder(
+        ///general spacing per resource
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 15, mainAxisSpacing: 15),
+        itemCount: list.length,
+
+        ///to avoid the scroll
+        physics: NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          ///calls the custom widget with the item parameters
+          return OnlineResourcesGridItem(
+              account: 'Platzi/live',
+              type: 'youtube',
+              description:
+                  'Tenim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,sed quia conseunde omnis iste natus error sit voluptatem accusantium doloremque');
+        },
+      ),
     );
   }
 }
---
+++ lib/shared/widgets/online-resources-grid-item_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:lab_movil_2222/themes/textTheme.dart';
+
+class OnlineResourcesGridItem extends StatelessWidget {
+  final String account;
+  final String type;
+  final String description;
+
+  const OnlineResourcesGridItem(
+      {Key? key,
+      required this.account,
+      required this.type,
+      required this.description})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        print('recurso online presionado');
+      },
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              image: AssetImage('assets/icons/${type}_icon.png'),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              account,
+              style: korolevFont.headline6?.apply(fontSizeFactor: 0.7),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              description,
+              style: korolevFont.headline6?.apply(fontSizeFactor: 0.5),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 6,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
