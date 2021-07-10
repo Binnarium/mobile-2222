161aee56639efb34a648179bfe28f3b85908b42a
Danthology96 <daniel.andres.novillo@gmail.com>
2021-07-09 22:59:07 -0500

    feature(History.model): Removing unnecesary code, making the constructor private. feature(content.screen): Implementing the new model ContentDto. feature(VideoPodcast.model): Removing old model and implementing solid design.

--- lib/shared/models/History.model.dart
+++ lib/shared/models/History.model.dart
@@ -1,58 +1,7 @@
-import 'dart:convert';
-
-HistoryModel historyModelFromJson(String str) =>
-    HistoryModel.fromJson(json.decode(str));
-
-String historyModelToJson(HistoryModel data) => json.encode(data.toJson());
-
-class HistoryModel {
-  HistoryModel({
-    this.height,
-    required this.kind,
-    this.name,
-    this.path,
-    this.url,
-    this.width,
-    this.title,
-    this.text,
-  });
-
-  int? height;
-  String kind;
-  String? name;
-  String? path;
-  String? url;
-  int? width;
-  String? title;
-  String? text;
-
-  factory HistoryModel.fromJson(Map<String, dynamic> json) => HistoryModel(
-        height: json["height"],
-        kind: json["kind"],
-        name: json["name"],
-        path: json["path"],
-        url: json["url"],
-        width: json["width"],
-        title: json["title"],
-        text: json["text"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "height": height,
-        "kind": kind,
-        "name": name,
-        "path": path,
-        "url": url,
-        "width": width,
-        "title": title,
-        "text": text,
-      };
-}
-
 abstract class HistoryDto {
   final String kind;
 
-  HistoryDto({
+  HistoryDto._({
     required this.kind,
   });
 
@@ -82,7 +31,7 @@ class TitleHistoryDto extends HistoryDto {
   TitleHistoryDto({
     required String kind,
     this.title,
-  }) : super(kind: kind);
+  }) : super._(kind: kind);
 }
 
 class TextHistoryDto extends HistoryDto {
@@ -91,7 +40,7 @@ class TextHistoryDto extends HistoryDto {
   TextHistoryDto({
     required String kind,
     this.text,
-  }) : super(kind: kind);
+  }) : super._(kind: kind);
 }
 
 class ImageHistoryDto extends HistoryDto {
@@ -100,5 +49,5 @@ class ImageHistoryDto extends HistoryDto {
   ImageHistoryDto({
     required String kind,
     this.url,
-  }) : super(kind: kind);
+  }) : super._(kind: kind);
 }
