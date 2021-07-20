5525c70a4e7609bcaaeb8f9b850fbec1ff8fce19
Bruno Esparza <bruno.be81@gmail.com>
2021-07-19 19:41:15 -0500

    add services to load dtos

---
+++ lib/services/load-login-information.service.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lab_movil_2222/services/i-load-information.service.dart';
+import 'package:lab_movil_2222/shared/models/Login.model.dart';
+
+class LoadLoginInformationService extends ILoadInformationService<LoginDto> {
+  @override
+  Future<LoginDto> load() async {
+    final snap = await FirebaseFirestore.instance
+        .collection('cities')
+        .doc('welcome')
+        .get();
+
+    if (!snap.exists) new ErrorDescription('Document welcome does not exists');
+
+    final Map<String, dynamic> payload = snap.data() as Map<String, dynamic>;
+
+    final result = LoginDto.fromJson(payload);
+    return result;
+  }
+}
