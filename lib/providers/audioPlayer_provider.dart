2ee17f3337d984b66019252c774718180a4e227d
Bruno Esparza <bruno.be81@gmail.com>
2021-08-18 02:05:54 -0500

    add player

--- lib/providers/audioPlayer_provider.dart
+++ lib/providers/audioPlayer_provider.dart
@@ -1,104 +1,158 @@
+import 'dart:math';
+
 import 'package:audio_session/audio_session.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:lab_movil_2222/models/asset.dto.dart';
+import 'package:rxdart/rxdart.dart';
+
+class PositionData {
+  final Duration position;
+  final Duration bufferedPosition;
+  final Duration duration;
+
+  PositionData({
+    required this.position,
+    required this.bufferedPosition,
+    required this.duration,
+  });
+}
+
+enum PlayerState {
+  paused,
+  playing,
+  completed,
+  loading,
+}
 
 class AudioPlayerProvider with ChangeNotifier {
   /// manage controls
-  final AudioPlayer player = AudioPlayer();
+  final AudioPlayer _player = AudioPlayer();
 
   AudioDto? _currentAudio;
 
+  AudioDto? get currentAudio => this._currentAudio;
+
   // late AudioSession player;
   AudioPlayerProvider() {
-    AudioSession.instance.then(
-      (session) =>
-          // Inform the operating system of our app's audio attributes etc.
-          // We pick a reasonable default for an app that plays speech.
-          session.configure(AudioSessionConfiguration.speech()),
-    );
+    AudioSession.instance.then((session) {
+      // Inform the operating system of our app's audio attributes etc.
+      // We pick a reasonable default for an app that plays speech.
+      session.configure(AudioSessionConfiguration.speech());
+    });
 
     // Listen to errors during playback.
-    player.playbackEventStream.listen((event) {},
+    _player.playbackEventStream.listen((event) {},
         onError: (Object e, StackTrace stackTrace) {
       print('A stream error occurred: $e');
     });
   }
 
-  Future<bool> setAudio(AudioDto audio) async {
+  Future<void> setAudio(AudioDto audio) async {
     this._currentAudio = audio;
+
     try {
       /// load audio to player
-      await player.setAudioSource(AudioSource.uri(Uri.parse(audio.url)));
-      playOrPause();
-      return true;
+      await _player.setUrl(audio.url);
+      await _player.play();
+      notifyListeners();
     } catch (e) {
       print("Error loading audio source: $e");
-      return false;
-    } finally {
-      notifyListeners();
     }
   }
 
-  Stream<Duration?>? get duration$ => this.player.durationStream;
-
-  Stream<Duration>? get position$ => this.player.positionStream;
-
-  AudioDto? get current => this._currentAudio;
-
-  ///TODO: method to close the player
+  /// method to stop and close player, if you want to only stop the current audio
+  /// use the [playOrPause] method
   close() {
-    this.player.stop();
+    this._player.stop();
     // this.player.dispose();
     this._currentAudio = null;
+    notifyListeners();
   }
 
-  /// method to rewind 5 secs
-  Future rewind5Seconds() async => (_goToPosition((currentPosition) {
-        if (currentPosition - Duration(seconds: 5) <= Duration.zero) {
-          return Duration.zero;
-        }
-        return currentPosition - Duration(seconds: 5);
-      }));
+  Future rewind(int seconds) async {
+    /// validate there is a current playing track
+    if (this._currentAudio == null) return;
 
-  /// method to forward to 5 seconds
-  Future forward5Seconds() async => (_goToPosition((currentPosition) {
-        if (currentPosition + Duration(seconds: 5) >= player.duration!) {
-          return player.duration!;
-        }
-        return currentPosition + Duration(seconds: 5);
-      }));
+    /// add bot positions
+    final int newSeconds = max(_player.position.inSeconds - seconds, 0);
+    final Duration newPosition = Duration(seconds: newSeconds);
+
+    /// move to position
+    await _player.seek(newPosition);
+  }
+
+  Future forward(int seconds) async {
+    /// validate there is a current playing track
+    if (this._currentAudio == null) return;
+
+    /// add bot positions
+    final int newSeconds = _player.duration == null
+        ? _player.position.inSeconds + seconds
+        : min(
+            _player.position.inSeconds + seconds, _player.duration!.inSeconds);
+    final Duration newPosition = Duration(seconds: newSeconds);
+
+    /// move to position
+    await _player.seek(newPosition);
+  }
 
   /// method to play or pause the audio
-  Future playOrPause() async {
-    if (player.playing) {
-      if (player.position >= player.duration!) {
-        player.seek(Duration.zero);
-        // _playIcon = Icon(Icons.pause_rounded);
-        player.play();
-      } else {
-        // _playIcon = Icon(Icons.play_arrow_rounded);
-        player.pause();
-      }
-    } else {
-      // _playIcon = Icon(Icons.pause_rounded);
-      player.play();
+  Future<void> playOrPause() async {
+    PlayerState? playing = await this.playing$.first;
+    print(playing);
+    print(playing);
+
+    /// if playing, then pause
+    if (playing == PlayerState.playing) {
+      await this._player.pause();
+      notifyListeners();
+      return;
+    }
+
+    /// if paused then play
+    if (playing == PlayerState.paused) {
+      await this._player.play();
+      notifyListeners();
+      return;
+    }
+
+    /// if replay, then move to start and play again
+    if (playing == PlayerState.completed) {
+      await this._player.seek(Duration.zero);
+      await this._player.play();
+      notifyListeners();
+      return;
     }
   }
 
-  /// method to change to a specyfic second (used on slider)
+  /// method to change to a specific second (used on slider)
   void changeToSecond(int second) {
     Duration newDuration = Duration(seconds: second);
-    player.seek(newDuration);
+    this._player.seek(newDuration);
   }
 
-  /// method used in forward, rewind, to get the new position in the audio
-  Future _goToPosition(
-    Duration Function(Duration currentPosition) builder,
-  ) async {
-    final currentPosition = player.position;
-    final newPosition = builder(currentPosition);
+  Stream<PositionData> get positionData$ =>
+      Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
+        this._player.positionStream,
+        this._player.bufferedPositionStream,
+        this._player.durationStream,
+        (position, bufferedPosition, duration) => PositionData(
+          position: position,
+          bufferedPosition: bufferedPosition,
+          duration: duration ?? Duration.zero,
+        ),
+      );
 
-    await player.seek(newPosition);
-  }
+  Stream<PlayerState> get playing$ =>
+      this._player.playerStateStream.map((event) {
+        final ProcessingState processingState = event.processingState;
+        if (processingState == ProcessingState.loading ||
+            processingState == ProcessingState.buffering)
+          return PlayerState.loading;
+        if (processingState == ProcessingState.completed)
+          return PlayerState.completed;
+        if (event.playing) return PlayerState.playing;
+        return PlayerState.paused;
+      });
 }
--- lib/shared/widgets/podcast_audioPlayer_widget.dart
+++ lib/shared/widgets/podcast_audioPlayer_widget.dart
@@ -1,10 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
 import 'package:lab_movil_2222/models/asset.dto.dart';
 import 'package:lab_movil_2222/providers/audioPlayer_provider.dart';
-import 'package:lab_movil_2222/shared/widgets/markdown.widget.dart';
+import 'package:lab_movil_2222/themes/colors.dart';
 import 'package:lab_movil_2222/themes/textTheme.dart';
 import 'package:provider/provider.dart';
 
@@ -15,7 +14,7 @@ class PodcastAudioPlayer extends StatefulWidget {
   PodcastAudioPlayer({
     Key? key,
     required this.audio,
-    required this.color,
+    this.color = Colors2222.primary,
   }) : super(key: key);
 
   @override
@@ -23,30 +22,16 @@ class PodcastAudioPlayer extends StatefulWidget {
 }
 
 class __PodcastAudioPlayerState extends State<PodcastAudioPlayer> {
-  Icon _playIcon = Icon(Icons.play_arrow_rounded);
   AudioPlayerProvider? _audioProviderInstance;
 
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // Release decoders and buffers back to the operating system making them
-    // available for other apps to use.
-    // this.audioProvider.close();
-    print("TODO: Audio provider disposed");
-
-    /// cant dispose, if this line is uncommented this will happen: next cities
-    /// podcast will have the same stop environment
-    // this.audioProvider.close();
-    super.dispose();
+  AudioPlayerProvider get audioProvider {
+    if (this._audioProviderInstance == null)
+      this._audioProviderInstance = Provider.of<AudioPlayerProvider>(context);
+    return this._audioProviderInstance!;
   }
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.only(bottom: 32),
       child: Column(
@@ -54,99 +39,151 @@ class __PodcastAudioPlayerState extends State<PodcastAudioPlayer> {
         mainAxisSize: MainAxisSize.max,
         children: [
           /// progress indicator
-          if (this.audioProvider.current?.path == this.widget.audio.path)
+          if (this.audioProvider.currentAudio?.path == this.widget.audio.path)
             Container(
               child: _AudioSlider(
-                duration$: this.audioProvider.duration$!,
-                progress$: this.audioProvider.position$!,
-                // moveTo: (position) {},
-                audioProvider: this.audioProvider,
+                positionData$: this.audioProvider.positionData$,
+                moveTo: this.audioProvider.changeToSecond,
               ),
             )
           else
             _FakeAudioSlider(),
 
           /// controls
-          if (this.audioProvider.current?.path == this.widget.audio.path)
+          if (this.audioProvider.currentAudio?.path == this.widget.audio.path)
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                // rewind button
-                _buildVideoButton(
-                    Icon(
-                      Icons.replay_5_rounded,
-                    ),
-                    // _rewind5Seconds,
-                    () {
-                  this.audioProvider.rewind5Seconds();
-                }, size),
+                /// rewind button
+                _PlayerControlIcon(
+                  color: this.widget.color,
+                  icon: Icons.replay_5_rounded,
+                  onPressed: () => this.audioProvider.rewind(5),
+                ),
 
-                // play button
-                _buildVideoButton(
-                    _playIcon, () => audioProvider.playOrPause(), size),
+                /// play button
+                _PlayButton(
+                  state$: this.audioProvider.playing$,
+                  color: this.widget.color,
+                  onPressed: () => this.audioProvider.playOrPause(),
+                ),
 
-                // forward button
-                _buildVideoButton(Icon(Icons.forward_5_rounded),
-                    () => this.audioProvider.forward5Seconds(), size),
+                /// forward button
+                _PlayerControlIcon(
+                  color: this.widget.color,
+                  icon: Icons.forward_5_rounded,
+                  onPressed: () => this.audioProvider.forward(5),
+                ),
               ],
             )
           else
 
             /// to initialize the audio
-            _buildVideoButton(_playIcon,
-                () => audioProvider.setAudio(this.widget.audio), size),
+            _PlayerControlIcon(
+              color: this.widget.color,
+              icon: Icons.play_arrow,
+              onPressed: () => audioProvider.setAudio(this.widget.audio),
+            ),
         ],
       ),
     );
   }
+}
 
-  //TODO: Change to class
-  Widget _buildVideoButton(Widget child, Function() onPressed, Size size) =>
-      Container(
-        // decoration: BoxDecoration(border: Border.all(color: Colors.white)),
-        width: size.width * 0.1,
-        height: size.height * 0.07,
-        child: ElevatedButton(
+class _PlayButton extends StatefulWidget {
+  final Stream<PlayerState> state$;
+  final Color color;
+  final VoidCallback onPressed;
+
+  _PlayButton({
+    Key? key,
+    required this.state$,
+    required this.color,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  __PlayButtonState createState() => __PlayButtonState();
+}
+
+const Map<PlayerState, IconData> PlayerStateIcons = {
+  PlayerState.playing: Icons.pause,
+  PlayerState.paused: Icons.play_arrow,
+  PlayerState.completed: Icons.replay,
+  PlayerState.loading: Icons.downloading,
+};
+
+class __PlayButtonState extends State<_PlayButton> {
+  StreamSubscription? _iconSub;
+  IconData icon = PlayerStateIcons[PlayerState.loading]!;
+
+  @override
+  void initState() {
+    super.initState();
+    this._iconSub = this.widget.state$.listen((event) {
+      print(event);
+      this.setState(() => this.icon = PlayerStateIcons[event]!);
+    });
+  }
+
+  @override
+  void dispose() {
+    this._iconSub?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _PlayerControlIcon(
+      color: this.widget.color,
+      icon: this.icon,
+      onPressed: () => this.widget.onPressed(),
+    );
+  }
+}
+
+class _PlayerControlIcon extends ElevatedButton {
+  _PlayerControlIcon({
+    Key? key,
+    required IconData icon,
+    required Color color,
+    required VoidCallback onPressed,
+  }) : super(
+          key: key,
+          child: Icon(icon),
           onPressed: onPressed,
-          child: child,
           style: ElevatedButton.styleFrom(
-            onPrimary: this.widget.color,
+            onPrimary: color,
             primary: Colors.white,
             alignment: Alignment.center,
             padding: EdgeInsets.all(0),
             shape: CircleBorder(),
           ),
-        ),
-      );
-
-  AudioPlayerProvider get audioProvider {
-    if (this._audioProviderInstance == null)
-      this._audioProviderInstance = Provider.of<AudioPlayerProvider>(context);
-    return this._audioProviderInstance!;
-  }
+        );
 }
 
 class _FakeAudioSlider extends _AudioSlider {
   _FakeAudioSlider()
       : super(
-          progress$: Stream.value(Duration(seconds: 0)),
-          duration$: Stream.value(Duration(seconds: 0)),
+          positionData$: Stream.value(
+            PositionData(
+              bufferedPosition: Duration(),
+              duration: Duration(),
+              position: Duration(),
+            ),
+          ),
         );
 }
 
 class _AudioSlider extends StatefulWidget {
-  final Stream<Duration> progress$;
-  final Stream<Duration?> duration$;
-  final AudioPlayerProvider? audioProvider;
+  final Stream<PositionData> positionData$;
 
   final Function(int)? moveTo;
 
   const _AudioSlider({
     Key? key,
-    required this.progress$,
-    required this.duration$,
+    required this.positionData$,
     this.moveTo,
-    this.audioProvider,
   }) : super(key: key);
 
   @override
@@ -154,28 +191,21 @@ class _AudioSlider extends StatefulWidget {
 }
 
 class __AudioSliderState extends State<_AudioSlider> {
-  Duration? currentProgress;
-  Duration? duration;
-  StreamSubscription? progressSub;
-  StreamSubscription? durationSub;
+  PositionData? positionData;
+  StreamSubscription? positionDataSub;
 
   @override
   void initState() {
     super.initState();
-    this.progressSub = this
+    this.positionDataSub = this
         .widget
-        .progress$
-        .listen((event) => this.setState(() => this.currentProgress = event));
-    this.durationSub = this
-        .widget
-        .duration$
-        .listen((event) => this.setState(() => this.duration = event));
+        .positionData$
+        .listen((event) => this.setState(() => this.positionData = event));
   }
 
   @override
   void deactivate() {
-    this.progressSub?.cancel();
-    this.durationSub?.cancel();
+    this.positionDataSub?.cancel();
     super.deactivate();
   }
 
@@ -187,19 +217,17 @@ class __AudioSliderState extends State<_AudioSlider> {
       children: [
         SliderTheme(
           data: SliderThemeData(
-            trackShape: CustomTrackShape(),
+            trackShape: _CustomTrackShape(),
           ),
           child: Slider(
             activeColor: Colors.white,
             inactiveColor: Colors.white.withOpacity(0.5),
             min: 0.0,
-            max: this.duration?.inSeconds.toDouble() ?? 0,
-            value: this.currentProgress?.inSeconds.toDouble() ?? 0,
-            onChanged: (double value) {
-              setState(() {
-                this.widget.audioProvider?.changeToSecond(value.toInt());
-                value = value;
-              });
+            max: this.positionData?.duration.inSeconds.toDouble() ?? 0,
+            value: this.positionData?.position.inSeconds.toDouble() ?? 0,
+            onChanged: (value) {
+              if (this.widget.moveTo != null)
+                this.widget.moveTo!(value.toInt());
             },
           ),
         ),
@@ -211,11 +239,11 @@ class __AudioSliderState extends State<_AudioSlider> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              currentProgress.toString().split(".")[0],
+              this.positionData?.position.toString().split(".")[0] ?? '0:00',
               style: korolevFont.bodyText2,
             ),
             Text(
-              duration.toString().split(".")[0],
+              this.positionData?.duration.toString().split(".")[0] ?? '0:00',
               style: korolevFont.bodyText2,
             ),
           ],
@@ -225,7 +253,7 @@ class __AudioSliderState extends State<_AudioSlider> {
   }
 }
 
-class CustomTrackShape extends RoundedRectSliderTrackShape {
+class _CustomTrackShape extends RoundedRectSliderTrackShape {
   Rect getPreferredRect({
     required RenderBox parentBox,
     Offset offset = Offset.zero,
