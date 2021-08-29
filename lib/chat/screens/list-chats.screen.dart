d5d42b460c56aa8bbc96f6820ad6c0ffce75ed4f
Bruno Esparza <bruno.be81@gmail.com>
2021-08-29 03:19:07 -0500

    add search chat

--- lib/chat/screens/list-chats.screen.dart
+++ lib/chat/screens/list-chats.screen.dart
@@ -3,9 +3,12 @@ import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:lab_movil_2222/chat/models/chat.model.dart';
-import 'package:lab_movil_2222/chat/models/player-search-result.model.dart';
+import 'package:lab_movil_2222/chat/models/search-chat-query.model%20copy.dart';
+import 'package:lab_movil_2222/chat/models/search-chat-response.model.dart';
 import 'package:lab_movil_2222/chat/screens/chat.screen.dart';
 import 'package:lab_movil_2222/shared/widgets/app-loading.widget.dart';
 import 'package:lab_movil_2222/shared/widgets/chapter-head-banner_widget.dart';
@@ -33,17 +36,20 @@ class ListChatsScreen extends StatefulWidget {
 class _ListChatsScreenState extends State<ListChatsScreen> {
   StreamSubscription? _chatsSub;
 
-  List<ChatModel>? chats;
+  List<ChatModel>? allChats;
 
   final TextEditingController _searchController = TextEditingController();
-  List<PlayerSearchResultModel>? playersResult;
+
+  /// list of chats founds
+  List<ChatModel?>? searchResults;
+
   @override
   void initState() {
     super.initState();
     _chatsSub = this
         .widget
         .userChats
-        .listen((event) => this.setState(() => this.chats = event));
+        .listen((event) => this.setState(() => this.allChats = event));
   }
 
   @override
@@ -89,69 +95,144 @@ class _ListChatsScreenState extends State<ListChatsScreen> {
               onValueChanged: (value) async {
                 if (value!.isEmpty) {
                   this.setState(() {
-                    this.playersResult = null;
+                    this.searchResults = null;
                   });
                   return;
                 }
                 HttpsCallable searchPlayers =
-                    FirebaseFunctions.instance.httpsCallable('searchPlayer');
-                final results = await searchPlayers<String>({"query": value});
+                    FirebaseFunctions.instance.httpsCallable('searchChat');
+                final SearchChatQueryModel query = SearchChatQueryModel(
+                  playerId: FirebaseAuth.instance.currentUser!.uid,
+                  query: value,
+                );
+                final results = await searchPlayers<String>(query.toMap());
                 final List<Map<String, dynamic>>? data =
                     (jsonDecode(results.data) as List<dynamic>)
                         .map((e) => e as Map<String, dynamic>)
                         .toList();
-                List<PlayerSearchResultModel>? playersResult = data
-                    ?.map(
-                      (dynamic e) => PlayerSearchResultModel.fromMap(e),
-                    )
+
+                print(data);
+                List<SearchChatResponseModel>? searchChatsResults = data
+                    ?.map((e) => SearchChatResponseModel.fromMap(e))
                     .toList();
 
                 this.setState(() {
-                  this.playersResult = playersResult;
+                  this.searchResults = searchChatsResults
+                      ?.map((searchResult) => this
+                          .allChats
+                          ?.firstWhere((chat) => chat.id == searchResult.id))
+                      .toList();
                 });
               },
               prefixIcon: Icons.search,
             ),
           ),
 
-          /// TODO:
-          if (this.playersResult != null) ...[
-            for (PlayerSearchResultModel player in this.playersResult!)
-              Padding(
-                padding: EdgeInsets.fromLTRB(sidePadding, 0, sidePadding, 20),
-                child: Text(player.displayName),
-              ),
+          /// searching results
+          if (this.searchResults != null) ...[
+            ChatListTitle(
+              title: 'Resultados de búsqueda',
+              context: context,
+            ),
+            for (ChatModel? chat in this.searchResults!)
+              if (chat != null)
+                ChatListItem(
+                  chat: chat,
+                  context: context,
+                ),
           ],
 
           /// if no chats loaded show loading icon
-          if (this.chats == null)
+          if (this.allChats == null)
             AppLoading()
 
           /// show a list of all chats
-          else
-            for (ChatModel chat in this.chats!)
-              ListTile(
-                ///TODO implementar esta linea cuando funcione lastMessage
-                /// trailing: Text('${chat.lastMessage!.sendedDate}'),
-                trailing: Text('16:50'),
-                leading: CircleAvatar(
-                  backgroundImage: AssetImage('assets/icons/avatar_icon.png'),
-                ),
-                title: Text('${chat.participantsNames}'),
-                subtitle: Text(
-                  chat.participantsNames,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                onTap: () => Navigator.of(context).pushNamed(
-                  ChatScreen.route,
-                  arguments: ChatScreen(
-                    chat: chat,
-                  ),
-                ),
+          else ...[
+            ChatListTitle(
+              title: 'Todas las conversaciones',
+              context: context,
+            ),
+            for (ChatModel chat in this.allChats!)
+              ChatListItem(
+                chat: chat,
+                context: context,
               ),
+          ],
         ],
       ),
     );
   }
+}
+
+class ChatListTitle extends ListTile {
+  ChatListTitle({
+    required String title,
+    required BuildContext context,
+  }) : super(
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.08,
+          ),
+          title: Row(
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              /// chat name
+              Expanded(
+                child: Text(
+                  title,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ),
+            ],
+          ),
+        );
+}
+
+class ChatListItem extends ListTile {
+  ChatListItem({
+    required ChatModel chat,
+    required BuildContext context,
+  }) : super(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/icons/avatar_icon.png'),
+          ),
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.08,
+          ),
+          title: Row(
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              /// chat name
+              Expanded(
+                child: Text(
+                  '${chat.chatName}',
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+
+              /// chat date
+              Text(
+                '${DateFormat('MM-dd HH:mm').format(chat.lastActivity)}',
+                style: Theme.of(context).textTheme.button,
+              ),
+            ],
+          ),
+
+          /// show last message chat
+          subtitle: Text(
+            chat.chatName,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+
+          /// when pressed then navigate to chats screens
+          onTap: () => Navigator.of(context).pushNamed(
+            ChatScreen.route,
+            arguments: ChatScreen(
+              chat: chat,
+            ),
+          ),
+        );
 }
