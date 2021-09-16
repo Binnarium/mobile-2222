f8aa876b5fcbe4ed868a9dd5738140d58789cc08
Bruno Esparza <bruno.be81@gmail.com>
2021-09-16 18:25:35 -0500

    update ios

--- lib/widgets/scaffold-2222/bottom-navigation-bar-widget.dart
+++ lib/widgets/scaffold-2222/bottom-navigation-bar-widget.dart
@@ -15,10 +15,11 @@ enum Lab2222NavigationBarPages {
 
 class Lab2222BottomNavigationBar extends StatelessWidget {
   const Lab2222BottomNavigationBar({
+    Key? key,
     this.activePage = Lab2222NavigationBarPages.home,
     this.nextPage,
     this.prevPage,
-  });
+  }) : super(key: key);
 
   final Lab2222NavigationBarPages? activePage;
 
@@ -31,60 +32,63 @@ class Lab2222BottomNavigationBar extends StatelessWidget {
       tag: 'navbar',
       child: Container(
         color: Colors2222.black,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Lab2222BottomNavigationBarItem>[
-            /// prev page item
-            Lab2222BottomNavigationBarItem(
-              onTap: prevPage,
-              icon: AssetImage('assets/navbar/chevron-left.png'),
-              active: true,
-            ),
+        child: SafeArea(
+          bottom: true,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Lab2222BottomNavigationBarItem>[
+              /// prev page item
+              Lab2222BottomNavigationBarItem(
+                onTap: prevPage,
+                icon: AssetImage('assets/navbar/chevron-left.png'),
+                active: true,
+              ),
 
-            /// home item
-            Lab2222BottomNavigationBarItem(
-              onTap: activePage == Lab2222NavigationBarPages.home
-                  ? () {}
-                  : () =>
-                      Navigator.pushReplacementNamed(context, HomeScreen.route),
-              icon: AssetImage('assets/navbar/boat.png'),
-              active: activePage == Lab2222NavigationBarPages.home,
-            ),
+              /// home item
+              Lab2222BottomNavigationBarItem(
+                onTap: activePage == Lab2222NavigationBarPages.home
+                    ? () {}
+                    : () => Navigator.pushReplacementNamed(
+                        context, HomeScreen.route),
+                icon: AssetImage('assets/navbar/boat.png'),
+                active: activePage == Lab2222NavigationBarPages.home,
+              ),
 
-            /// profile item
-            Lab2222BottomNavigationBarItem(
-              onTap: activePage == Lab2222NavigationBarPages.profile
-                  ? () {}
-                  : () => Navigator.pushNamed(context, ProfileScreen.route),
-              icon: AssetImage('assets/navbar/user.png'),
-              active: activePage == Lab2222NavigationBarPages.profile,
-            ),
+              /// profile item
+              Lab2222BottomNavigationBarItem(
+                onTap: activePage == Lab2222NavigationBarPages.profile
+                    ? () {}
+                    : () => Navigator.pushNamed(context, ProfileScreen.route),
+                icon: AssetImage('assets/navbar/user.png'),
+                active: activePage == Lab2222NavigationBarPages.profile,
+              ),
 
-            /// information item
-            Lab2222BottomNavigationBarItem(
-              onTap: activePage == Lab2222NavigationBarPages.information
-                  ? () {}
-                  : () => Navigator.pushNamed(context, WelcomeScreen.route),
-              icon: AssetImage('assets/navbar/info.png'),
-              active: activePage == Lab2222NavigationBarPages.information,
-            ),
+              /// information item
+              Lab2222BottomNavigationBarItem(
+                onTap: activePage == Lab2222NavigationBarPages.information
+                    ? () {}
+                    : () => Navigator.pushNamed(context, WelcomeScreen.route),
+                icon: AssetImage('assets/navbar/info.png'),
+                active: activePage == Lab2222NavigationBarPages.information,
+              ),
 
-            /// chat item
-            Lab2222BottomNavigationBarItem(
-              onTap: activePage == Lab2222NavigationBarPages.chat
-                  ? () {}
-                  : () => Navigator.pushNamed(context, ChatsScreen.route),
-              icon: AssetImage('assets/navbar/message-square.png'),
-              active: activePage == Lab2222NavigationBarPages.chat,
-            ),
+              /// chat item
+              Lab2222BottomNavigationBarItem(
+                onTap: activePage == Lab2222NavigationBarPages.chat
+                    ? () {}
+                    : () => Navigator.pushNamed(context, ChatsScreen.route),
+                icon: AssetImage('assets/navbar/message-square.png'),
+                active: activePage == Lab2222NavigationBarPages.chat,
+              ),
 
-            /// text page item
-            Lab2222BottomNavigationBarItem(
-              onTap: nextPage,
-              icon: AssetImage('assets/navbar/chevron-right.png'),
-              active: true,
-            ),
-          ],
+              /// text page item
+              Lab2222BottomNavigationBarItem(
+                onTap: nextPage,
+                icon: AssetImage('assets/navbar/chevron-right.png'),
+                active: true,
+              ),
+            ],
+          ),
         ),
       ),
     );
