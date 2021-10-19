643a9ca294f31001a56e3ea84d9dc75cd053064b
Bruno Esparza <bruno.be81@gmail.com>
2021-10-19 00:09:04 -0500

    add load stage for player

--- lib/player/services/current-player.service.dart
+++ lib/player/services/current-player.service.dart
@@ -8,40 +8,62 @@ class CurrentPlayerService {
       : _firestore = FirebaseFirestore.instance,
         _auth = FirebaseAuth.instance;
 
+  /// [internal] track if service has been loaded information
+  bool _loadedConfiguration = false;
+
   /// services
   final FirebaseFirestore _firestore;
   final FirebaseAuth _auth;
 
-  /// Last player status cashed value
-  /// access to its value using the [CurrentPlayerService.currentPlayer]
-  static PlayerModel? _player;
+  /// cached information of player
+  ///
+  /// access sync value using the [currentPlayer] getter
+  PlayerModel? _player;
+
+  Future<void> load() async {
+    _loadedConfiguration = true;
+    player$.listen((player) {
+      _player = player;
+    });
+  }
+
+   Stream<PlayerModel?>? _playerSource$;
+
+  /// get stream value of player
+  Stream<PlayerModel?> get player$ {
+    /// make sure configuration was loaded
+    assert(
+      _loadedConfiguration,
+      'Player service was not loaded, load player using [CurrentPlayerService.load()] at the start of your application',
+    );
+    _playerSource$ ??= _auth
+        .userChanges()
+
+        /// point to player document
+        .map((user) => user == null
+            ? null
+            : _firestore.collection('players').doc(user.uid))
+
+        /// turn document to stream of player snapshots
+        .switchMap((doc) => doc == null ? Stream.value(null) : doc.snapshots())
+
+        /// turn snapshot into document data or null
+        .map((snapshot) => snapshot?.data())
+
+        /// turn snapshot data into player object, if props found
+        .map((data) => data == null ? null : PlayerModel.fromMap(data))
+        .shareReplay();
+
+    return _playerSource$!;
+  }
 
   /// get sync access to the current signed in player
-  static PlayerModel? get currentPlayer => CurrentPlayerService._player;
-
-  Stream<PlayerModel?> get player$ => _auth
-          .userChanges()
-
-          /// point to player document
-          .map((user) => user == null
-              ? null
-              : _firestore.collection('players').doc(user.uid))
-
-          /// turn document to stream of player snapshots
-          .switchMap((playerDoc) =>
-              playerDoc == null ? Stream.value(null) : playerDoc.snapshots())
-
-          /// turn snapshot into document data or null
-          .map((snapshot) => snapshot?.data())
-
-          /// turn snapshot data into player object, if props found
-          .map((object) {
-        if (object == null) {
-          CurrentPlayerService._player = null;
-          return null;
-        } else {
-          CurrentPlayerService._player = PlayerModel.fromMap(object);
-          return CurrentPlayerService._player;
-        }
-      }).shareReplay();
+  PlayerModel? get currentPlayer {
+    /// make sure configuration was loaded
+    assert(
+      _loadedConfiguration,
+      'Player service was not loaded, load player using [CurrentPlayerService.load()] at the start of your application',
+    );
+    return _player;
+  }
 }
